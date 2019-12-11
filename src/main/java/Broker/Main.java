0a417a2d5ce91d01df29011cacfdeb0e84fac840
dimpho Putu <diputu@student.wethinkcode.co.za>
2019-12-11 16:49:48 +0200

    yoh

--- src/main/java/Broker/Main.java
+++ src/main/java/Broker/Main.java
@@ -1,4 +1,4 @@
-package za.co.diputu.Broker;
+package Broker;
 
 import java.io.*;
 import java.net.*;
@@ -9,12 +9,10 @@ public class Main {
     public static void main(String[] args) throws Exception{
         //String.valueOf(idCounter)
          Socket s = new Socket(InetAddress.getByName("localhost"), port);
-         String str = "Hello za.co.diputu.Broker";
+         String str = "Hello Broker";
          OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
          PrintWriter out = new PrintWriter(os);
          os.write(str);
          os.flush();
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(idCounter + i);
     }
 }
--- src/main/java/NIOsockets/NonBlockingServer.java
+++ src/main/java/NIOsockets/NonBlockingServer.java
@@ -3,15 +3,19 @@ package NIOsockets;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NonBlockingServer {
     private static Selector selector = null;
 
     public static void main(String[] args) {
-        try{
+        try {
             selector = Selector.open();
 
             ServerSocketChannel socket = ServerSocketChannel.open();
@@ -20,15 +24,57 @@ public class NonBlockingServer {
             socket.configureBlocking(false);
             int ops = socket.validOps();
             socket.register(selector, ops, null);
-            while (true)
-            {
+            while (true) {
                 selector.select();
-                Set<SelectionKey> selectedKeys
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> i = selectedKeys.iterator();
+
+                while (i.hasNext()) {
+                    SelectionKey key = i.next();
+
+                    if (key.isAcceptable()) {
+                        handleAccept(socket, key);
+                    }
+                    else if (key.isReadable()) {
+                        handleRead(key);
+                    }
+                    i.remove();
+                }
             }
 
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void handleAccept(ServerSocketChannel mySocket, SelectionKey key) throws IOException {
+
+        System.out.println("Connection Accepted...");
+
+        // Accept the connection and set non-blocking mode
+        SocketChannel client = mySocket.accept();
+        client.configureBlocking(false);
+
+        // Register that client is reading this channel
+        client.register(selector, SelectionKey.OP_READ);
+    }
+
+    private static void handleRead(SelectionKey key) throws IOException {
+        System.out.println("Reading...");
+
+        SocketChannel client = (SocketChannel) key.channel();
+
+        // Create buffer to read data
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        client.read(buffer);
+//        Parse data from buffer to String
+        String data = new String(buffer.array()).trim();
+        if (data.length() > 0) {
+            System.out.println("Received message: " + data);
+            if (data.equalsIgnoreCase("exit")) {
+                client.close();
+                System.out.println("Connection closed...");
+            }
         }
     }
 }
